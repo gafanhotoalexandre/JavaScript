f3d4ed65c727525352f647abd2be7a97d90b7eb2
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2020-07-31 16:25:43 -0300

    Experimentos

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var velocidade = 31
+if (velocidade <= 30) {
+    console.log('Você é muito vagaroso')
+}else {
+    console.log('Vai com calma, irmão')
+}
+console.log('Use sempre cinto de segurança')
