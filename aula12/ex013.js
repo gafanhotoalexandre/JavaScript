3f9ca20fdece939aaea4f9cd1154d80d47939ddd
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2020-08-02 22:50:30 -0300

    Condicoes multiplas

---
+++ aula12/ex013.js
@@ -0,0 +1,41 @@
+var agora = new Date()
+var dia = agora.getDay()
+
+/*
+    0 = DOMINGO
+    1 = SEGUNDA
+    2 = TERÇA
+    3 = QUARTA
+    4 = QUINTA
+    5 = SEXTA
+    6 = SÁBADO
+*/
+
+var diaSemana
+switch(dia) {
+    case 0:
+       diaSemana = 'Domingo'
+       break
+    case 1:
+        diaSemana = 'Segunda-feira'
+        break
+    case 2:
+        diaSemana = 'Terca-feira'
+        break
+    case 3:
+        diaSemana = 'Quarta-feira'
+        break
+    case 4:
+        diaSemana = 'Quinta-feira'
+        break
+    case 5:
+        diaSemana = 'Sexta-feira'
+        break
+    case 6:
+        diaSemana = 'Sábado'
+        break
+    default:
+        console.log('ERRO| Impossível realizar ação')
+        break
+}
+console.log(diaSemana)
