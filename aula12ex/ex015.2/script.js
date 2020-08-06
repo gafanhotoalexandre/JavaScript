87f7f369c0f88f0819198aab40aa430f8231f090
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2020-08-05 23:08:05 -0300

    Definindo alguns pontos

--- aula12ex/ex015.2/script.js
+++ aula12ex/ex015.2/script.js
@@ -7,8 +7,40 @@ function verificar() {
     if (fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     }else {
+        //Recolhendo Dados
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        res.innerHTML = `Idade calculada: ${idade} anos`
+        var genero = ""
+        //Criando Atributo "Foto"
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        //Organizando Fotos
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 13) {
+                //CRIANÇA
+                img.setAttribute('src', )
+            }else if (idade <= 20) {
+                //JOVEM
+            }else if (idade <= 55) {
+                //ADULTO
+            }else if (idade > 55) {
+                //IDOSO
+            }
+        }else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 13) {
+                //CRIANÇA
+            }else if (idade <= 20) {
+                //JOVEM
+            }else if (idade <= 55) {
+                //ADULTO
+            }else if (idade > 55) {
+                //IDOSO
+            }
+        }
+
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} de ${idade} anos`
     }
 }
