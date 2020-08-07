d0dc093e168fe8200d1dc699281bd925e3924e69
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2020-08-07 18:04:33 -0300

    Iniciando uma tabuada

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,10 @@
-alert('Olá, Mundo!')
+function tabuada() {
+    var num = document.querySelector('input#txtN')
+    var tab = document.querySelector('select#selTab')
+
+    if (num.value.length == 0) {
+        let n = Number(num.value)
+    }else {
+        window.alert('Por favor, insira um número!')
+    }
+}
