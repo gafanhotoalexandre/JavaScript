c2b651b64adcf4b37e8731d71fe32eb40e6d7cdd
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2020-08-06 21:02:05 -0300

    atualizacao do ambiente

---
+++ aula13/ambiente1.js
@@ -0,0 +1,12 @@
+var a = 10
+if (a === 1) {
+    while (a <= 10) {
+        console.log(a)
+        a++
+    }
+}else if(a === 10) {
+    while (a >= 1) {
+        console.log(a)
+        a--
+    }
+}
---
+++ aula14/ambiente.js
@@ -0,0 +1,5 @@
+console.log('Vai Começar...')
+for (let i = 1; i <= 5; i++) {
+    console.log(`Estrutura FOR ${i}`)
+}
+console.log('Fim!')
