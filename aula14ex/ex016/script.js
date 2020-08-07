d5c3ab4991bbc37d0a32a2f4a9cbf34465f7d7b5
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2020-08-07 17:51:57 -0300

    Super Contador realizado

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,1 +1,34 @@
-alert('Olá, Mundo!')
+function contar() {
+    let inicio = document.getElementById('txtI')
+    let fim = document.getElementById('txtF')
+    let passo = document.getElementById('txtP')
+    let res = document.getElementById('res')
+
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        alert('[ERRO] Faltam dados...')
+        res.innerHTML = 'Impossível contar...'
+    }else {
+        res.innerHTML = 'Contando...<br/>'
+        let  i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        //Configurando condições de PASSO
+        if (p <= 0) {
+            window.alert('Passo Inválido! Considerando PASSO 1')
+            p = 1
+        }
+
+        if (i < f) {
+            //CONTAGEM CRESCENTE
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}` 
+            }
+        }else {
+            //CONTAGEM REGRESSIVA
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
+}
