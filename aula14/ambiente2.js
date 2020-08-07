38fe90fb45500fbe36330e10da5b8c1b8fee7813
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2020-08-06 21:22:42 -0300

    Contador Inteligente

---
+++ aula14/ambiente2.js
@@ -0,0 +1,12 @@
+// FOR | Contador Inteligente
+var inicio = 10
+var fim = 5
+if (inicio < fim) {
+    for (let i = inicio; i <= fim; i++) {
+        console.log(i)
+    }
+}else if (inicio > fim) {
+    for(let i = inicio; i >= fim; i--) {
+        console.log(i)
+    }
+}
