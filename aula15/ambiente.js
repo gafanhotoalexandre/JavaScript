609f9f018274734f24f4f9b584119026e32e6c91
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2020-08-07 22:14:58 -0300

    ambiente 01

--- aula15/ambiente.js
+++ aula15/ambiente.js
@@ -4,7 +4,7 @@ num.sort()
 console.log(`Nosso vetor é o ${num} e possui o comprimento de ${num.length}`)*/
 
 let n = [5, 17, 2, 9, 3]
-
+console.log(n)
 for (let i = 0; i < n.length; i++) {
-    console.log(n[i])
+    console.log(`A posição ${i} tem o valor ${n[i]}`)
 }
