cee194b8bc67e25c36ef229d400b144cd597a064
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2020-08-08 21:21:40 -0300

    up

---
+++ aula16/funcao03.js
@@ -0,0 +1,8 @@
+let v = function mult(n) {
+    return n * 2
+}
+/* ADICIONAMOS UMA VARIÁVEL, PORÉM, O log FICA MAIS BONITINHO 
+let r = v(5)
+console.log(r)
+*/
+console.log(v(5))
---
+++ aula16/funcao04.js
@@ -0,0 +1,20 @@
+/*function fatorial (n) {
+    let f = 1
+    let fat = ""
+    for (let i = n; i >= 1; i--) {
+        f *= i
+        
+    }
+    return fat
+}
+let r = fatorial(5)
+console.log(r)*/
+/*
+function fatorial(n) {
+    if (n == 1) {
+        return 1
+    }else {
+        return n * fatorial(n - 1)
+    }
+}
+console.log(fatorial(5))*/
